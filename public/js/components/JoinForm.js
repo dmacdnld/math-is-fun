7bf8d48ba1c9e252371754886556beed820bf165
Daniel MacDonald <dnmacdnld@gmail.com>
2014-04-28 20:17:30 -0500

    Adding error for no name

--- public/js/components/JoinForm.js
+++ public/js/components/JoinForm.js
@@ -9,31 +9,40 @@ module.exports = React.createClass({
     var that = this;
 
     socket.on('player:invalid', function () {
-      that.setState({ playerInvalid: true });
+      that.setState({ invalidReason: 'taken' });
     });
   },
 
   getInitialState: function () {
-    return { playerInvalid: false };
+    return { invalidReason: null };
   },
 
   submitForm: function (event) {
     event.preventDefault();
 
     var name = this.refs.name.getDOMNode().value.trim();
+
+    if (name === '') {
+      return this.setState({ invalidReason: 'missing' });
+    }
+
     return socket.emit('player:applied', name);
   },
 
   render: function () {
-    var errorMessageStyle = {
-      display: this.state.playerInvalid ? '' : 'none'
+    var invalidReason = this.state.invalidReason;
+    var errorMessages = {
+      taken: "Sorry, that name is taken",
+      missing: "Sorry, we need a name"
     };
+    var errorMessage = errorMessages[invalidReason];
+    var errorStyle = invalidReason ? {} : { display: 'hidden' };
 
     return (
       <form onSubmit={this.submitForm}>
-        <label htmlFor='name'>Pick a name</label><br />
+        <label htmlFor='name'>Name</label><br />
         <input id='name' type='text' placeholder='e.g. Mathemagician' ref='name'/><br />
-        <div style={errorMessageStyle}>Name already taken</div>
+        <div style={errorStyle}>{ errorMessage }</div>
         <button type='submit'>Join the game!</button>
       </form>
     );
